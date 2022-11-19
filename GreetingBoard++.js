f32f309dbc7634c58799ab90d5116cb3488fa233
khorlmills <109895517+khorlmills@users.noreply.github.com>
2022-11-19 08:07:47 +0800

    alert added

--- GreetingBoard++.js
+++ GreetingBoard++.js
@@ -1,14 +1,22 @@
-//Simple Program that uses push function
+//Simple Program that uses push function and alert function
 
 let arrHolder = ["Hello"];
+let arrHolderOutput = [];
 console.log("\t\t\t\t Greeting Board ! for everyone to see");
+console.log(arrHolder);
 var loopNum = prompt("Input the amount of greetings you want to add to the message board");
 
 for(var i = 0; i < loopNum; i++)
 {
     arrHolder.push(prompt("Enter the greeting that you want to add")); 
-   
+    arrHolderOutput.push(arrHolder[i+1]);
 }
 
 console.log("\t\t\t\t Greeting Board ! for everyone to see");
-console.log(arrHolder)
+console.log(arrHolder);
+
+alert(`Messages: [${arrHolderOutput}] are added to the message board`);
+
+
+
+
