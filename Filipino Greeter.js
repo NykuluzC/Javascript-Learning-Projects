fff1a3361f51c7478580edb0dc7a8c5028370261
khorlmills <109895517+khorlmills@users.noreply.github.com>
2022-11-20 08:02:35 +0800

    Add files via upload

---
+++ Filipino Greeter.js
@@ -0,0 +1,10 @@
+
+var nationality = "Filipino";
+
+var response = prompt("What nationality am I from?", "Chinese");
+
+if (response === "Filipino"){
+    alert("Yes I am " + nationality + " ! " + "Mabuhay ! :)");
+}else{
+    alert("No I am not " + response + " ! " + "Gago ! :(");
+}
